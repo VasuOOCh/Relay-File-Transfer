bc9416baabb03bf7a6f75f7042800302e77de1e4
VasuOOCh <vasuc.ug23.cs@nitp.ac.in>
2024-12-20 13:22:33 +0530

    init

---
+++ src/RelayClient.java
@@ -0,0 +1,155 @@
+import javax.swing.*;
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class RelayClient implements Runnable {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String clientId;
+    private static Scanner sc = new Scanner(System.in);
+    private boolean running = false;
+
+    public static void main(String[] args) {
+        System.out.println("Type the server address to connect");
+        String serverAdd = sc.nextLine();
+
+        RelayClient client = new RelayClient();
+        client.connect(serverAdd, 8080);
+        client.running = true;
+        //  we want to start a thread to listen for incoming messages
+        new Thread(client).start();
+
+        System.out.println("Type SEND <targetId> to send a file or EXIT to leave connection");
+
+        while(true) {
+            String cmd = sc.nextLine();
+
+            if(cmd.startsWith("SEND")) {
+                String[] parts = cmd.split(" ");
+                String targetId = parts[1];
+                client.sendFile(client.selectFile(), targetId);
+            } else if (cmd.equals("EXIT")) {
+                System.out.println("Closing connection...");
+                try {
+                    client.socket.close();
+                    client.running = false;
+//                    sc.close();
+                    System.exit(0);
+                }
+                catch (IOException e) {
+                    e.getMessage();
+                    e.printStackTrace();
+                }
+            }else {
+                System.out.println("Type a valid command");
+            }
+
+        }
+    }
+
+
+    public void connect(String serverAdd, int PORT) {
+        try {
+            socket = new Socket(serverAdd.trim(), PORT);;
+            System.out.println("Connection made with server "+ serverAdd);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            String msg =in.readUTF();
+            if(msg.startsWith("ID:")) {
+                String[] parts = msg.split(" ");
+                clientId = parts[1];
+                System.out.println("My client ID : " + clientId);
+            }
+
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while(running) {
+            try {
+                String cmd = in.readUTF();
+                if(cmd.startsWith("RECEIVING")) {
+                    String[] parts = cmd.split(" ");
+                    String filename = parts[1];
+                    long fileSize = Long.parseLong(parts[2]);
+                    System.out.println("Receiving file "+ filename + " size : " + fileSize + " bytes");
+
+                    receiveFile(fileSize, filename);
+                }
+            }
+            catch (IOException e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public File selectFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(null);
+        if(result == JFileChooser.APPROVE_OPTION) {
+            return fileChooser.getSelectedFile();
+        }else {
+            return null;
+        }
+    }
+
+    public void sendFile(File file, String targetId) {
+        try {
+
+            if(file == null) {
+                System.out.println("Select a valid file");
+                return;
+            }
+
+            FileInputStream fileInputStream = new FileInputStream(file);
+            out.writeUTF("SEND "+ targetId.trim() + " " + file.getName());
+            out.writeLong(file.length());
+
+            byte[] b = new byte[8192]; // external buffer size set to 8KB
+            long remaining = file.length();
+
+            while(remaining > 0) {
+                int read = fileInputStream.read(b, 0, (int)Math.min(b.length, remaining));
+                out.write(b, 0 ,read);
+                remaining -= read;
+            }
+
+            System.out.println("File sent successfully");
+
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void receiveFile(long fileSize, String filename) {
+        try {
+            File file = new File(filename);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            long remaining = fileSize;
+            byte[] b = new byte[8192];
+
+            while (remaining > 0) {
+                int read = in.read(b, 0, (int)Math.min(b.length, remaining));
+                fileOutputStream.write(b,0,read);
+                remaining -= read;
+            }
+
+            System.out.println("File received successfully");
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/RelayServer.java
@@ -0,0 +1,131 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RelayServer {
+    private static final int PORT = 8080;
+    public static Map<String,ConnectionHandler> clients = new ConcurrentHashMap<>();
+    // we use ConcurrentHashMaps for better Thread compatibility and scalability
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Connection established");
+            while(true) {
+
+                System.out.println("Looking for clients...");
+
+                // for listening to the client connections
+                Socket clientSocket = serverSocket.accept();
+                String clientId = UUID.randomUUID().toString();
+
+                // we need to start a thread for the connected client
+
+                System.out.println("Client "+ clientId +" connected");
+
+                ConnectionHandler connectionHandler = new ConnectionHandler(clientSocket, clientId);
+                clients.put(clientId,connectionHandler);
+
+                // Send Client its client ID
+                connectionHandler.sendMessage("ID: " + clientId);
+
+                // What is connectionHandler : It is a class for creating objects for individual
+                // clients to handle them and also a runnable for a thread, which will be initialized for
+                // each client
+
+                new Thread(connectionHandler).start();
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
+
+class ConnectionHandler implements Runnable {
+    private Socket clientSocket;
+    private String clientId;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public ConnectionHandler(Socket clientSocket, String clientId) {
+        this.clientSocket = clientSocket;
+        this.clientId = clientId;
+        try {
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String message) {
+        //  this method will send a message to this connected client
+        try{
+            out.writeUTF(message);
+            out.flush();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        // here we will listen for file sending commands from the client
+        // command : SEND <clientId> <Filename>
+        try {
+            while(true) {
+                String cmd = in.readUTF();
+                if(cmd.startsWith("SEND")) {
+                    String[] parts = cmd.split(" ");
+                    String targetId = parts[1];
+                    String filename = parts[2];
+
+                    // Reading file data
+                    long fileSize = in.readLong();
+                    sendFile(targetId, filename, fileSize);
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void sendFile(String targetId, String filename, long fileSize) {
+        try {
+            ConnectionHandler targetConnection = RelayServer.clients.get(targetId);
+            if(targetConnection == null ) {
+                sendMessage("ERROR Target_Client_Not_Found");
+                return;
+            }
+
+            targetConnection.sendMessage("RECEIVING "+ filename + " " + fileSize);
+
+            byte[] buffer = new byte[8192];
+            long remaining = fileSize;
+
+            while (remaining > 0) {
+                int read = in.read(buffer, 0, (int)Math.min(buffer.length, remaining));
+                targetConnection.out.write(buffer, 0, read);
+                remaining -= read;
+            }
+            targetConnection.out.flush();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
